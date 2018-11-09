bc2b28afbee0ab99e633c27fd0a4ed073e3871b0
Puppy <cutiepuppy707@list.ru>
2018-11-09 21:41:09 +0300

    Update massive.cpp

--- massive.cpp
+++ massive.cpp
@@ -2,48 +2,22 @@
 
 int main(){
 	int a;
-	int b=-1; //пор€дковый номер €чейки
-	int m[a];
-	int g;//число в €чейке b
-	int i; // счетчик
+	int n; // number we need to find
 	printf("How many numbers you will have in massive? \n");
-	scanf("%d", &a); // задаем число обьектов в массиве
-	printf("Numbers in your massive will be in order? \n Print number, bigger than 0 if yes, print 0 if no. \n");
-	int t;
-	scanf("%d", &t);
-	if(t>0){
-		printf("Type your minimum number \n");
-		scanf("%d", &g);
-		g-=1;
-		for(i=0; i<a; i++){
-			g+=1;
-			b+=1;
-			printf("m[%d]=%d \n" ,b ,g);
-	}	
-	}
-	else{
-		b=-1; // номер €чейки
-		g=0; // число в €чейке
-		i=0;
-		printf("Tell numbers in massive \n");
+	scanf("%d", &a); // max number of massive
+	printf("Type number you want to find in massive \n");
+	scanf("%d", &n);
+	printf("Tell numbers in massive \n");
+	int m[a];
+	bool k = false;
+	int i=0; // count, number of massive
 		while(i<a){
-			b+=1;
+			printf("m[%d]=",i); //m[0] ect;
+			scanf("%d", &m[i]);
+			m[i]==n;
+			if(m[i]==n) k=true;
 			i+=1;
-			printf("m[%d]=" , b); //€чейка массива
-			scanf("%d", &g);
 		}
-	}
-		int n; // число которое нужно найти в массиве
-		printf("Type number you want to find in massive \n");
-		scanf("%d", &n); // m[b]=g
-		i=0;
-		for(i=0;i<a;i++){
-			m[b]==n;
-	}
-	if(m[b]==n){
-		printf("number %d exists in massive m" ,n);		
-	}
-	else{
-		printf("number %d does not exist in massive m" ,n);
-	}
+		if(k)printf("number %d exists in massive",n);
+		if(!k) printf("number %d does not exists in massive",n);
 }
