4614aa2b797a97f7f069fba56157a27b1c12b1c9
Puppy <cutiepuppy707@list.ru>
2018-11-26 20:10:19 +0300

    Update IAmATree.cpp

--- IAmATree.cpp
+++ IAmATree.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	int n=0; // m[n]
-	int r=0; //ðåçåðâ
+	int r=0; //Ã°Ã¥Ã§Ã¥Ã°Ã¢
 	ifstream puppy;
 	puppy.open("input.txt");
 	while(!puppy.eof()){
@@ -15,28 +15,54 @@ int main(){
 	cout << n << endl;
 	int m[n];
 	int i=0;
+	int min=INT_MAX;
+	int max=-min;
 	ifstream lel;
 	lel.open("input.txt");
-	int *last;
 	while(i<n){
 		lel >> m[i];
-		if(n-i==1)*last = m[i];
+		if(m[i]>max)max=m[i];
+		if(m[i]<min)min=m[i];
 		i++;
 	}
-	int *p = m;
-    cout << *p << endl;
-	cout << *last << endl;
+	i=0;
+	r=min;
+	int rr=r-1;
+	int g=max+1-min;
+	int c[g];
+	int k=0;
+	while(rr<max){
+		c[k]=0;
+		while(i<n){
+			r==m[i];
+			if(r==m[i])c[k]+=1;
+			i+=1;
+		}
+		k+=1;
+		r+=1;
+		rr+=1;
+		i=0;
+	}
+	rr=0;
+	k=0;
+	int p=0; //?aca?a n[k]
+	i=0;
+	while(rr<g){
+		p=c[k];
+		while(i<p){
+			cout << min;
+			i+=1;
+		}
+		min+=1;
+		rr+=1;
+		i=0;
+		k+=1;
+	}
+	cout << "\n" << endl;
+	int *point = m;
 	printf("Which number ya wanna find in massive? \n");
 	int find=0;
 	cin>>find;
-	bool noodle = false;
-	r=0;
-	/*if(n%2==0){
-		r=n/2;
-		i=r-1;
-		m[i]==find;
-		if(m[i]>find){
-			*last=m[i];
-		}
-	}*/
+	i=0;
+   	
 }
